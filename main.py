b6ffecee1d367164b6d2d216c2504c0ee99577c7
Hub5on <haibert.hs32@gmail.com>
2023-07-01 21:16:13 +0200

    refactor: use new class Snake

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from turtle import Turtle, Screen
+from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
-starting_positions = [(0,0), (-20,0), (-40,0)]
+snake = Snake()
 
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.goto(position)
-    
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+        
 screen.exitonclick()
